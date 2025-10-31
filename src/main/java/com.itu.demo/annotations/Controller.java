da07526bf525d4685575554fdb6cd35cdf5529e9
Ocyyy <riantsoaoceane03@gmail.com>
2025-10-31 22:04:42 +0300

    Sprint2-bis

---
+++ src/main/java/com.itu.demo/annotations/Controller.java
@@ -0,0 +1,14 @@
+package com.itu.demo.annotations;
+
+import java.lang.annotation.Documented;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RUNTIME)
+@Target(TYPE)
+public @interface Controller {
+    String value() default "";
+}
---
+++ src/main/java/com.itu.demo/controllers/UtilityClass.java
@@ -0,0 +1,8 @@
+package com.itu.demo.controllers;
+
+// Pas de @Controller ici, donc ignorée par le scanner
+public class UtilityClass {
+    public void helper() {
+        System.out.println("Helper method");
+    }
+}
